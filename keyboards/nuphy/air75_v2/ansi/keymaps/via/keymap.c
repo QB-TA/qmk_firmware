edcdc4f281f5084bffbfa57b710a925e0bff868c
Binh Ta <binh.ta.94@gmail.com>
2025-06-09 09:35:41 +0200

    correct keymap

--- keyboards/nuphy/air75_v2/ansi/keymaps/via/keymap.c
+++ keyboards/nuphy/air75_v2/ansi/keymaps/via/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     // layer win
-    [8] = LAYOUT_75_ansi(
+    [4] = LAYOUT_75_ansi(
         KC_ESC,     KC_F1,      KC_F2,      KC_F3,     KC_F4,      KC_F5,       KC_MPLY,   KC_MUTE,    KC_VOLD,     KC_VOLU,   KC_F10,     KC_F11,     KC_F12,      _______,    _______,    _______,
         KC_GRV,     KC_1,      KC_2,        KC_3,      KC_4,       KC_5,        KC_6,      KC_7,       KC_8,        KC_9,      KC_0,       KC_MINS,    KC_EQL,      KC_BSPC,                _______,
         KC_TAB,     KC_Q,      KC_W,        KC_E,      KC_R,       KC_T,        KC_Y,      KC_U,       KC_I,        KC_O,      KC_P,       KC_LBRC,    KC_RBRC,                 KC_BSLS,    _______,
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     // layer win Fn
-    [9] = LAYOUT_75_ansi(
+    [5] = LAYOUT_75_ansi(
         _______,    KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,       KC_F6,     KC_F7,      KC_F8,       KC_F9,     KC_F10,     KC_F11,     KC_F12,      _______,   _______,    _______,
         _______,    LNK_BLE1,   LNK_BLE2,   LNK_BLE3,   LNK_RF,     _______,     _______,   _______,    _______,     _______,   _______,    _______,	_______,     KC_DEL,                _______,
         _______,    _______,    _______,    _______,    _______,    _______,     RGB_SPI,   RGB_RMOD,   RGB_VAI,     RGB_HUI,   RGB_SAI,    _______,    _______,                BAT_NUM,    _______,
